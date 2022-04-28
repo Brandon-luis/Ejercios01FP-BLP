8c6b0a39304fea529f654b3d0c939c5128c2fae5
unknown <vrgitoblu09222.com>
2022-04-28 09:27:53 -0500

    mi primer trabajo

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+public class HolaMundo{
+
+public satic void main(String[] blP){
+//Variables y Datos de Entrada 
+int lA=4;
+int lB=5;
+int lC=3;
+//Proceso
+int v=lA*lB*lC;
+//datos salida 
+syatem.out.println("El volumen es: "+v);
+
+}
+
+}
