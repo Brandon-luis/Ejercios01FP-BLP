6d6cd34eeecec7a64490d9b1313dc78dcb0df0e0
brandon louis <vegito2020migate@gmail.com>
2022-05-12 15:15:55 +0000

    Lol

---
+++ EstRepetitivas/EjerciciosRe.java
@@ -0,0 +1,17 @@
+package EstRepetitivas;
+
+public class EjerciciosRe {
+
+  public static void imprimir_10While(){
+       int numInit=1;
+    while(numInit<=10){
+        System.out.print(numInit);
+       numInit=numInit+1;
+    }
+  }
+
+public static void main(String[] args) {
+    imprimir_10While();
+        
+    }
+}
