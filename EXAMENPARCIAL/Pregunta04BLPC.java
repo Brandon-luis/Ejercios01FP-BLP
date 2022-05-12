a8be54ebbbf983a15a99721be778f4e0870f9226
brandon louis <vegito2020migate@gmail.com>
2022-05-12 23:41:21 +0000

    EXAMEN PARCIAL

---
+++ EXAMENPARCIAL/Pregunta04BLPC.java
@@ -0,0 +1,153 @@
+package EXAMENPARCIAL;
+
+import java.util.Scanner;
+/**
+ * Ejercicio01-02-03-04BLPC
+ */
+
+public class Pregunta04BLPC {
+    static Scanner lt=new Scanner(System.in);
+    public static void pregunta03BLPC() {
+        //Variables
+        int n1, n2;
+        System.out.println("Ingrese el primer valor");
+        n1=lt.nextInt();
+        System.out.println("Ingrese el segundo valor (Para hallar el modulo 2do valor no cuenta)");
+        n2=lt.nextInt();
+        System.out.println("Ingrese el signo");
+        char operador=lt.next().charAt(0);
+
+        //Proceso 
+
+        switch (operador) {
+            case '+': System.out.println(n1+n2);  break;
+            case '-': System.out.println(n1-n2);  break;
+            case '*': System.out.println(n1*n2);  break;
+            case '/': System.out.println(n1/n2);  break;
+            case 'p':Math.pow(n1, n2);break;
+            case 'R':Math.pow(n1,(1/n2));break;
+            case '%': System.out.println(n1%2);  break;
+           default: break;
+        }
+    }
+    public static void pregunta02BLPC() {
+        //Variables
+       
+        double mb, descuento=0, IT;
+
+        System.out.println("Ingresar Monto Base");
+        mb=lt.nextDouble();
+        System.out.println("----------------------------------------------------");
+        System.out.println("----------------------------------------------------");
+        System.out.println("+ El Descuento Dependiento Del monto con IGV");
+
+        //Proceso
+        double MBIGV = (mb * 18)/100;
+        double IGV = (mb + MBIGV);
+
+        if (IGV>2000) {
+            descuento=IGV*10/100; 
+        } else if(IGV<=2000 && IGV>1000){
+            descuento=IGV*5/100;
+        } else if(IGV<=1000 && IGV>500){
+            descuento=IGV*2/100;
+        } else {
+            descuento=IGV*0;
+        }
+        if (IGV>2000){
+            IT = (mb - IGV*10/100);
+        } else if(IGV<=2000 && IGV>1000){
+            IT = (mb - IGV*5/100);
+        } else if(IGV<=1000 && IGV>500){
+            IT = (mb- IGV*2/100);
+        } else {
+            IT = (mb - IGV);
+        }
+        
+        //Datos de Salida
+        System.out.println("El importe Total es:"+IT);
+        
+    }
+    public static void pregunta01BLPC() {
+        //Variables
+
+        String EstudianteNv;
+        double ECres, EPres, TPres; 
+        
+        System.out.println("Ingrese la nota del Examen de Conocimiento");
+        ECres=lt.nextDouble();
+        System.out.println("----------------------------------------------------");
+        System.out.println("Ingrese la nota de la Entrevista Personal");
+        EPres=lt.nextDouble();
+        System.out.println("----------------------------------------------------");
+        System.out.println("Ingrese la nota del Test Psicologico");
+        TPres=lt.nextDouble();
+        
+    
+        //Proceso
+
+        double ECpuntuacion = (ECres * 40)/100;
+        double EPpuntuacion = (EPres * 35)/100;
+        double TPpuntuacion = (TPres * 25)/100;
+        double promedio = (ECpuntuacion + EPpuntuacion + TPpuntuacion);
+
+        if (promedio>=17 && promedio<=20) {
+            EstudianteNv="Su nivel es 4";
+        } else if(promedio<17 && promedio>=14){
+            EstudianteNv="Su nivel es 3";
+        } else if(promedio<14 && promedio>=11){
+            EstudianteNv="Su nivel es 2";
+        } else if(promedio<11){
+            EstudianteNv="No obtiene vacante porque su nivel es 1";
+        } else {
+            EstudianteNv="Ingrese notas en la escala vigesimal";
+        }
+        // Datos de Salida
+        System.out.println(EstudianteNv);
+    }
+    public static void pregunta04BLPC() {
+        //Variables
+          int edad;
+          String g;
+          String c; 
+          //Datos de Entrada
+          System.out.print("Ingrese la edad:");
+          edad=lt.nextInt();
+          System.out.println("----------------------------------------------------");
+          System.out.println("Ingrese el genero (F=femenino y M=masculino)");
+          g=lt.next();
+          //Proceso
+          if (g=="M") {
+              System.out.println("Eres un hombre");
+          } else if(g=="F"){
+              System.out.println("Eres una mujer");
+          }
+          if (edad>70) {
+              c="Le corresponde la vacuna tipo C";
+          } else if(edad>16 && edad<70){
+              c="Le corresponde la vacuna tipo B"; 
+          } else if(edad>16 && edad<70){
+              c="Le corresponde la vacuna tipo C";
+          } else {
+              c="Le corresponde la vacuna tipo A";
+          }    
+          //Datos de Salida 
+          System.out.println(c);
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Ingrese la pregunta");
+            int pregunta = lt.nextInt();
+            switch (pregunta) {
+              case 1:
+                pregunta01BLPC(); break;
+              case 2:
+                pregunta02BLPC(); break;
+              case 3:
+                pregunta03BLPC(); break;  
+              case 4:
+                pregunta04BLPC(); break;          
+            }      
+    }
+}
